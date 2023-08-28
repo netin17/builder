5914b684148ecc8f94df9c0a7eef6adc62b87d9f
nitin sharma <nitinsharma.sharma6@gmail.com>
2023-08-28 17:29:28 +0530

    new update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['127.0.0.1'], // Add your local domain here
+        domains: ['https://estateon.com'], // Add your local domain here
       },
 }
 
